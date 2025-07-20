babe02663a635397d84614fa0eef1a5746fe4481
Mohit Nautiyal <88551782+MOHIT3014@users.noreply.github.com>
2025-07-21 00:33:40 +0530

    completed

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.getElementById("display");
+let btn=document.querySelectorAll(".btn");
+
+let str="";
+
+let arr = Array.from(btn);
+arr.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") {
+            str = eval(str);
+            input.value = str;
+        }
+        else if (e.target.innerHTML == "C") {
+            str = "";
+            input.value = str;
+        }
+        else if (e.target.innerHTML == "Del") {
+            str = str.slice(0, -1);
+            input.value = str;
+        }
+        else {
+            str += e.target.innerHTML;
+            input.value = str;
+        }
+    })
+})
